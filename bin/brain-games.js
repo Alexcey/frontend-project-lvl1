d1f15fd14859d9ba26ad703318c9a21588cd4b5a
Alexcey <alexcey007@yandex.ru>
2021-02-28 12:31:24 +0300

    step_4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
+/* eslint-disable no-console, import/extensions */
 
-import welcome from "../src/cli.js";
+import welcome from '../src/cli.js';
 
 welcome();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
-import readlineSync from "readline-sync";
+/* eslint-disable no-console, import/extensions */
+import readlineSync from 'readline-sync';
 
 export default () => {
-  console.log("Welcome to the Brain Games!");
-  const userName = readlineSync.question("May I have your name? ");
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
